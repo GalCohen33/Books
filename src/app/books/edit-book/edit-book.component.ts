1313e5064c9dbe84cbbd3dfa6ac54243d2e03677
Gal Cohen <gal.cohen@sap.com>
2022-04-11 14:53:57 +0300

    ###Not sure

---
+++ src/app/books/edit-book/edit-book.component.ts
@@ -0,0 +1,30 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {Book} from "../models/book.model";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-edit-book',
+  templateUrl: './edit-book.component.html',
+  styleUrls: ['./edit-book.component.css']
+})
+export class EditBookComponent implements OnInit {
+  //book:Book | undefined;
+
+  constructor(
+    public dialogRef: MatDialogRef<EditBookComponent>,
+    @Inject(MAT_DIALOG_DATA) public book:Book,) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  submit(f:NgForm){
+    if(!f.valid)
+      return;
+
+    //state change -> update
+
+  }
+
+}
